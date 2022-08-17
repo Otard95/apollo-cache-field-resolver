06da339994cddd13e45bac5dbe9297b0dc5f1fc7
arealmaas <arealmaas@gmail.com>
2022-08-17 15:43:02 +0200

    cleanup

--- tests/redis-key-value-cache.spec.ts
+++ tests/redis-key-value-cache.spec.ts
@@ -1,60 +1,51 @@
-import childProcess from 'child_process'
-import redis from 'ioredis'
-import casual from 'casual'
-import RedisKeyValueCache from '../src/redis-key-value-cache';
+const redis = require('ioredis-mock')
+import RedisKeyValueCache from '../src/redis-key-value-cache'
 
-beforeAll(() => {
-  childProcess.execSync('docker run -p 6379:6379 -d redis:6.2.6-alpine')
-})
-afterAll(() => {
-  childProcess.execSync(`docker ps | grep 'redis:6.2.6-alpine' | awk '{print $1}' | xargs docker kill`)
-})
-
-const newRedis = () => new redis({
-  port: 6379,
-  host: 'localhost',
-  connectionName: casual.uuid,
-})
+let newRedis: any
+let cache: RedisKeyValueCache
 
 describe('InMemoryCache', () => {
+  beforeEach(async() => {
+    newRedis = new redis()
+    cache = new RedisKeyValueCache({ client: newRedis })
+  })
+
+  afterEach(async() => {
+    await new redis().flushall()
+  })
 
   describe('When getting unset key', () => {
-    const cache = new RedisKeyValueCache({ client: newRedis() })
     it('should return null', () => {
-      expect(cache.get('key')).resolves.toBeNull()
+      expect(cache.get('key')).resolves.toBeUndefined()
     })
   })
 
   describe('When getting a set key', () => {
-    const cache = new RedisKeyValueCache({ client: newRedis() })
-    cache.set('key', 'value', 1000)
+    beforeEach(async() => {
+      await cache.set('key', 'value', 1000)
+    })
     it('should return stored value', () => {
       expect(cache.get('key')).resolves.toEqual('value')
     })
   })
 
   describe('After deleting a key', () => {
-    const cache = new RedisKeyValueCache({ client: newRedis() })
-    it('.get(...) should return null', () => {
-      // await cache.set('key', 'value', 1000)
-      // await cache.delete('key')
-      expect(cache.get('key')).resolves.toBeNull()
+    it('.get(...) should return null', async() => {
+      await cache.set('key', 'value', 1000)
+      await cache.delete('key')
+      expect(cache.get('key')).resolves.toBeUndefined()
     })
   })
 
   describe('When setting a key with ttl', () => {
-    const spy = jest.spyOn(global.Date, 'now')
-    spy.mockImplementationOnce(() => 0)
-
-    const cache = new RedisKeyValueCache({ client: newRedis() })
-    cache.set('key', 'value', 10)
-    it('should return the value before the ttl expires', () => {
-      spy.mockImplementationOnce(() => 0)
+    it('should return the value before the ttl expires', async() => {
+      await cache.set('key', 'value', 10)
       expect(cache.get('key')).resolves.toEqual('value')
     })
-    it('should return null after the ttl expires', () => {
-      spy.mockImplementationOnce(() => 15000)
-      expect(cache.get('key')).resolves.toBeNull()
+    it('should return null after the ttl expires', async() => {
+      await cache.set('key', 'value', 1)
+      await new Promise(resolve => setTimeout(resolve, 1200))
+      expect(cache.get('key')).resolves.toBeUndefined()
     })
   })
 })
