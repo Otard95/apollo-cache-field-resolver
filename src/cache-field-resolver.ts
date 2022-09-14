338267ec3917880f1b058d6620e6e2a39bc85fad
StianM <stian.myklebostad@schibsted.com>
2022-09-14 08:05:32 +0200

    BREAKING CHANGE: Restructure field resolver
    Instead of juggling args and types, just export a function taking
    options returning a function taking resolver and so on.

--- src/cache-field-resolver.ts
+++ src/cache-field-resolver.ts
@@ -2,58 +2,35 @@ import { Required } from 'utility-types'
 import InMemoryCache from "./in-memory-cache"
 import { resolveCacheKey } from './cache-key'
 import { CacheOptions, GQLResolver } from './types'
+import generateSessionId from './session'
 
 const defaultOptions = {
   cacheKey: resolveCacheKey,
   cache: new InMemoryCache(),
   cacheNull: false,
+  logger: console,
 }
 
-function cacheFieldResolver<
+const cacheFieldResolver = <
   P extends { [argName: string]: unknown },
   C,
   A extends { [argName: string]: unknown },
   R
 >(
+  opts?: CacheOptions<P, C, A>,
+) => (
   resolver: GQLResolver<P, C, A, R>
-): GQLResolver<P, C, A, R>;
-function cacheFieldResolver<
-  P extends { [argName: string]: unknown },
-  C,
-  A extends { [argName: string]: unknown },
-  R
->(
-  opts: CacheOptions<P, C, A>,
-  resolver: GQLResolver<P, C, A, R>
-): GQLResolver<P, C, A, R>;
-
-function cacheFieldResolver<
-  P extends { [argName: string]: unknown },
-  C,
-  A extends { [argName: string]: unknown },
-  R
->(
-  optsOrResolver: CacheOptions<P, C, A> | GQLResolver<P, C, A, R>,
-  resolver?: GQLResolver<P, C, A, R>
-): GQLResolver<P, C, A, R> {
-  let opts: CacheOptions<P, C, A> | null = null
-  let resolverFn: GQLResolver<P, C, A, R>
-
-  if (resolver === undefined) {
-    resolverFn = optsOrResolver as GQLResolver<P, C, A, R>
-  } else {
-    opts = optsOrResolver as CacheOptions<P, C, A>
-    resolverFn = resolver as GQLResolver<P, C, A, R>
-  }
+): GQLResolver< P, C, A, R> => {
 
   const options = { ...defaultOptions, ...(opts || {}) } as
     CacheOptions<P, C, A>
     & Pick<
       Required<CacheOptions<P, C, A>>,
-      'cacheKey' | 'cache'
+      'cacheKey' | 'cache' | 'logger'
     >
 
   const cacheResolver: GQLResolver<P, C, A, R> = async (parent, args, context, info) => {
+
     const cache = typeof options.cache === 'function'
       ? options.cache(context)
       : options.cache
@@ -66,8 +43,8 @@ function cacheFieldResolver<
       options.cacheKey(options, sessionId, info, parent, args)
 
     if (cacheKey === null || cacheKey.length <= 0) {
-      (options.logger || console).warn('[cacheFieldResolver](SKIP) could not create cache key')
-      return resolverFn(parent, args, context, info)
+      options.logger.warn('[cacheFieldResolver](SKIP) could not create cache key')
+      return resolver(parent, args, context, info)
     }
 
     try {
@@ -75,16 +52,16 @@ function cacheFieldResolver<
       if (cachedValue !== undefined) {
         const parsedValue = JSON.parse(cachedValue)
         if (parsedValue !== undefined && (options.cacheNull || parsedValue !== null)) {
-          (options.logger || console).debug(`[cacheFieldResolver] cache hit on key ${cacheKey}`)
+          options.logger.debug(`[cacheFieldResolver] cache hit on key ${cacheKey}`)
           return parsedValue
         }
       }
-      (options.logger || console).debug(`[cacheFieldResolver] cache miss on key ${cacheKey}`)
+      options.logger.debug(`[cacheFieldResolver] cache miss on key ${cacheKey}`)
     } catch (e) {
-      (options.logger || console).debug(`[cacheFieldResolver] cache value parse error ${cacheKey}`)
+      options.logger.debug(`[cacheFieldResolver] cache value parse error ${cacheKey}`)
     }
 
-    const res = await resolverFn(parent, args, context, info)
+    const res = await resolver(parent, args, context, info)
 
     const maxAge = cacheHint && cacheHint.maxAge
     if (
@@ -96,14 +73,16 @@ function cacheFieldResolver<
       )
       && typeof maxAge === 'number' && maxAge > 0
     ) {
-      (options.logger || console).debug(`[cacheFieldResolver] cached value on key ${cacheKey}`)
+      options.logger.debug(`[cacheFieldResolver] cached value on key ${cacheKey}`)
       await cache.set(cacheKey, JSON.stringify(res), { ttl: maxAge })
     }
 
     return res
+
   }
 
   return cacheResolver
+
 }
 
 export default cacheFieldResolver
