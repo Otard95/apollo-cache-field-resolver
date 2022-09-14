c3963b4a5e36209411538ce80e61afc4033bbbe8
StianM <stian.myklebostad@schibsted.com>
2022-09-14 07:59:36 +0200

    feat: Add the required schema to the package exposrts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as cacheFieldResolver } from './cache-field-resolver';
 export { InMemoryCache, RedisKeyValueCache } from './cache';
+export { typeDef } from './schema'
---
+++ src/schema.ts
@@ -0,0 +1,14 @@
+import { gql } from 'apollo-server-core'
+
+export const typeDef = gql`
+  enum CacheControlScope {
+    PUBLIC
+    PRIVATE
+  }
+
+  directive @cacheControl(
+    maxAge: Int
+    scope: CacheControlScope
+    inheritMaxAge: Boolean
+  ) on FIELD_DEFINITION | OBJECT | INTERFACE | UNION
+`
