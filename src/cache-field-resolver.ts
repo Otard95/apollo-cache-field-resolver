1556d029477c9a03475540d45d97b7878f664499
StianM <stian.myklebostad@schibsted.com>
2022-09-14 13:48:44 +0200

    feat: Revert to function overrides

--- src/cache-field-resolver.ts
+++ src/cache-field-resolver.ts
@@ -11,16 +11,42 @@ const defaultOptions = {
   logger: console,
 }
 
-const fieldCacheResolver = <
+function fieldCacheResolver<
   P extends { [argName: string]: unknown },
   C,
   A extends { [argName: string]: unknown },
   R
 >(
-  opts?: CacheOptions<P, C, A>,
-) => (
   resolver: GQLResolver<P, C, A, R>
-): GQLResolver< P, C, A, R> => {
+): GQLResolver<P, C, A, R>;
+function fieldCacheResolver<
+  P extends { [argName: string]: unknown },
+  C,
+  A extends { [argName: string]: unknown },
+  R
+>(
+  opts: CacheOptions<P, C, A>,
+  resolver: GQLResolver<P, C, A, R>
+): GQLResolver<P, C, A, R>;
+
+function fieldCacheResolver<
+  P extends { [argName: string]: unknown },
+  C,
+  A extends { [argName: string]: unknown },
+  R
+>(
+  optsOrResolver: CacheOptions<P, C, A> | GQLResolver<P, C, A, R>,
+  resolver?: GQLResolver<P, C, A, R>
+): GQLResolver< P, C, A, R> {
+  let opts: CacheOptions<P, C, A> | null = null
+  let resolverFn: GQLResolver<P, C, A, R>
+
+  if (resolver === undefined) {
+    resolverFn = optsOrResolver as GQLResolver<P, C, A, R>
+  } else {
+    opts = optsOrResolver as CacheOptions<P, C, A>
+    resolverFn = resolver as GQLResolver<P, C, A, R>
+  }
 
   const options = { ...defaultOptions, ...(opts || {}) } as
     CacheOptions<P, C, A>
@@ -48,8 +74,8 @@ const fieldCacheResolver = <
       options.cacheKey(options, sessionId, info, parent, args, context)
 
     if (cacheKey === null || cacheKey.length <= 0) {
-      options.logger.warn('[cacheFieldResolver](SKIP) could not create cache key')
-      return resolver(parent, args, context, info)
+      options.logger.warn('[fieldCacheResolver](SKIP) could not create cache key')
+      return resolverFn(parent, args, context, info)
     }
 
     try {
@@ -57,16 +83,16 @@ const fieldCacheResolver = <
       if (cachedValue !== undefined) {
         const parsedValue = JSON.parse(cachedValue)
         if (parsedValue !== undefined && (options.cacheNull || parsedValue !== null)) {
-          options.logger.debug(`[cacheFieldResolver] cache hit on key ${cacheKey}`)
+          options.logger.debug(`[fieldCacheResolver] cache hit on key ${cacheKey}`)
           return parsedValue
         }
       }
-      options.logger.debug(`[cacheFieldResolver] cache miss on key ${cacheKey}`)
+      options.logger.debug(`[fieldCacheResolver] cache miss on key ${cacheKey}`)
     } catch (e) {
-      options.logger.debug(`[cacheFieldResolver] cache value parse error ${cacheKey}`)
+      options.logger.debug(`[fieldCacheResolver] cache value parse error ${cacheKey}`)
     }
 
-    const res = await resolver(parent, args, context, info)
+    const res = await resolverFn(parent, args, context, info)
 
     const maxAge = cacheHint && cacheHint.maxAge
     if (
@@ -78,7 +104,7 @@ const fieldCacheResolver = <
       )
       && typeof maxAge === 'number' && maxAge > 0
     ) {
-      options.logger.debug(`[cacheFieldResolver] cached value on key ${cacheKey}`)
+      options.logger.debug(`[fieldCacheResolver] cached value on key ${cacheKey}`)
       await cache.set(cacheKey, JSON.stringify(res), { ttl: maxAge })
     }
 
