679b96883596a0b222e7b36fea95e9e51c28e8b7
StianM <stian.myklebostad@schibsted.com>
2022-09-16 14:30:25 +0200

    feat: Add exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export { default as fieldCacheResolver } from './cache-field-resolver'
 export { default as referenceCacheResolver } from './cache-reference-resolver'
 export { InMemoryCache, RedisKeyValueCache } from './cache'
+export {
+  setDefaultOption,
+  appendDefaultOption,
+  clearDefaultOption,
+} from './options'
 export { typeDef } from './schema'
